cc9c25a59c8048d7be376460dc1d8d8a312a66c0
Brandon Kriesch <bkriesch@liftbrands.com>
2017-11-27 13:29:48 -0600

    cleanup and readd slack notifications

--- jenkins-agents-cluster/services/agents/nugetbuilder/latest-repo-pkg-version.sh
+++ jenkins-agents-cluster/services/agents/nugetbuilder/latest-repo-pkg-version.sh
@@ -1,8 +1,1 @@
 echo $(mono /root/nuget.exe list $1 -Source https://sonatype-nexus.fod247.io/repository/nuget-hosted/| head -n 1 | cut -d' ' -f2)
-
-# getLatestPackage () {
-#     latestPackage=$(mono /root/nuget.exe list $1 -Source https://api.nuget.org/v3/index.json | head -n 1 | cut -d' ' -f2) #sed -n -e 1p
-    
-#     echo "$latestPackage"
-# }
-# getLatestPackage $1
