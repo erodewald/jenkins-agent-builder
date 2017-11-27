c37802a57c7bdacc82228015d120661638b79696
Brandon Kriesch <bkriesch@liftbrands.com>
2017-11-27 17:34:48 -0600

    handle use case where package not first added

--- jenkins-agents-cluster/services/agents/nugetbuilder/latest-repo-pkg-version.sh
+++ jenkins-agents-cluster/services/agents/nugetbuilder/latest-repo-pkg-version.sh
@@ -1,1 +1,6 @@
-echo $(mono /root/nuget.exe list $1 -Source https://nexus.fod247.io/repository/nuget-hosted/| head -n 1 | cut -d' ' -f2)
+response=$(mono /root/nuget.exe list $1 -Source https://nexus.fod247.io/repository/nuget-hosted/| head -n 1)
+if [ "$response" == "No packages found." ]; then
+    echo 0.0.0
+else
+    echo "$response" | cut -d' ' -f2
+fi
